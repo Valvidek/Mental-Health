4b9c1111be25d386bed47de7e3b115f0c14529f5
Zoki-design <batzorig23f@gmail.com>
2025-05-25 16:50:08 +0800

    user

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,11 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  hasAnsweredQuestions: { type: Boolean, default: false },
+const UserSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  mood: String,
+  date: { type: Date, default: Date.now },
 });
 
-export default mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
